201ba84a4074eaa4f84779058fb2d94ea1233434
Collin Didier <collin.didier80@gmail.com>
2020-11-22 13:26:46 -0600

    add server.js with middleware proxy to avoid cors issues

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const logger = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const {createProxyMiddleware} = require('http-proxy-middleware');
+const apiProxy = createProxyMiddleware({
+  target: 'https://swapi.dev/api/starships',
+  changeOrigin: true, // for vhosted sites, changes host header to match to target's host
+  logLevel: 'debug',
+});
+const PORT = process.env.PORT || 3007
+const app = express()
+
+app.use(logger('dev'))
+app.use(helmet())
+// app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', apiProxy)
+app.listen(PORT, async () => {
+  try {
+    console.log(`App listening on port: ${PORT}`)
+  } catch (error) {
+    throw new Error('Connection Error')
+  }
+})
+
