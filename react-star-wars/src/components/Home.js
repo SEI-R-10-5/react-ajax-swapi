60334a916b2bea6bae52dbeafe3e0cc6d871d40d
Jady Hom <hyjady@gmail.com>
2020-11-21 19:35:51 -0500

    added div and imported css

--- react-star-wars/src/components/Home.js
+++ react-star-wars/src/components/Home.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/App.css'
 
 const Home = (props) => {
   return (
     <div>
       <header>
-          <h1>STAR WARS STARSHIPS</h1>
+        <h1>STAR WARS STARSHIPS</h1>
       </header>
 
-      {props.starships.map(starship => {
-        let location = {
-          pathname: '/starship',
-          state: starship
-        }
-        return (
-          <Link
-            to={location}
-            key={starship.name}
-          >
-          <div className="starship">
-            {starship.name}
-          </div>
-          </Link>
-        )
-      })}
+      <div className="starship-container">
+        {props.starships.map(starship => {
+          let location = {
+            pathname: '/starship',
+            state: starship
+          }
+          return (
+            <Link
+              to={location}
+              key={starship.name}
+            >
+              <div className="starship">
+                <h3>{starship.name}</h3>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
--- react-star-wars/src/components/StarshipPage.js
+++ react-star-wars/src/components/StarshipPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../styles/App.css'
 
 const StarshipPage = (props) => {
   let starship = props.location.state
@@ -10,22 +11,21 @@ const StarshipPage = (props) => {
         <h1>STAR WARS STARSHIPS</h1>
       </header>
 
-      <div className="starship-data">
-        <h4>Name: {starship.name}</h4>
-        <h4>Model: {starship.model}</h4>
-        <h4>Manufacturer: {starship.manufacturer}</h4>
-        <h4>Starship Class: {starship.starship_class}</h4>
-        <h4>Max Atmosphering Speed: {starship.max_atmosphering_speed}</h4>
-        <h4>Crew Members: {starship.crew}</h4>
-        <h4>Passengers: {starship.passengers}</h4>
-        <h4>Cargo Capacity: {starship.cargo_capacity}</h4>
+      <div className="data-container">
+        <div className="starship-data">
+          <h4>Name:<br />{starship.name}</h4>
+          <h4>Model:<br />{starship.model}</h4>
+          <h4>Manufacturer:<br />{starship.manufacturer}</h4>
+          <h4>Starship Class:<br />{starship.starship_class}</h4>
+          <h4>Max Atmosphering Speed:<br />{starship.max_atmosphering_speed}</h4>
+          <h4>Crew Members:<br />{starship.crew}</h4>
+          <h4>Passengers:<br />{starship.passengers}</h4>
+          <h4>Cargo Capacity:<br />{starship.cargo_capacity}</h4>
+        </div>
+      </div>
 
-        <Link 
-          to="/"
-          className="returnhome"
-        > 
-          Return Home 
-        </Link>
+      <div className="returnhome">
+        <Link to="/"> Return Home </Link>
       </div>
     </div>
   )
